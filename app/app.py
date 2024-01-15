3ea426ef9c26430475da3fc2024201b00289f57e
mararo-ops <danielmararo1@gmail.com>
2024-01-15 07:48:39 +0300

    class  heroes

--- app/app.py
+++ app/app.py
@@ -4,3 +4,16 @@ from flask_cors import CORS
 from flask_restful import Api,Resource
 
 from models import db, Hero,HeroPowers,Power
+CORS(app)
+migrate = Migrate(app, db)
+
+db.init_app(app)
+api = Api(app)
+
+class HeroResource(Resource):
+    def get(self):
+      #getting the heroes
+      heros=Hero.query.all()
+
+      heroes_list = [{'id': hero.id, 'name': hero.name, 'super_name': hero.super_name} for hero in heros]
+      return make_response(jsonify(heroes_list),200)
