29179a7fe1d5ebaa81791fb46eaa4216dd91cd59
mararo-ops <danielmararo1@gmail.com>
2024-01-15 07:55:36 +0300

    heroes

--- app/models.py
+++ app/models.py
@@ -2,3 +2,19 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
 
 db = SQLAlchemy()
+
+class Hero(db.Model):
+    __tablename__ = 'hero'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String,nullable=False)
+    super_name=db.Column(db.String)
+    created_at=db.Column(db.DateTime,server_default=db.func.now())
+    updated_at=db.Column(db.DateTime,onupdate=db.func.now())
+
+    hero_powers = db.relationship('HeroPowers', back_populates='hero')
+
+   
+    def __repr__(self):
+        return f'<Hero {self.name} {self.super_name}>'
+    
