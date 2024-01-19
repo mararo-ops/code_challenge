2d9f3ee823cdebea5ee4d7ff5c54b7805a566f5c
mararo-ops <danielmararo1@gmail.com>
2024-01-19 16:38:56 +0300

    final comit

--- app/app.py
+++ app/app.py
@@ -1,9 +1,13 @@
 from flask import Flask, make_response, jsonify, request
 from flask_migrate import Migrate
+from flask_cors import CORS
 from flask_restful import Api, Resource
 
 from models import db, Hero, HeroPowers, Power
 
+app = Flask(__name__)
+CORS(app)
+migrate = Migrate(app, db)
 
 db.init_app(app)
 api = Api(app)
